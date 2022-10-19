0306511e346be7fe65f81b3f58272b77aa1d90dc
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2022-10-19 17:34:10 +1100

    Make no paragon limit appear in mod browser

--- NoParagonLimit/ModHelperData.cs
+++ NoParagonLimit/ModHelperData.cs
@@ -9,4 +9,7 @@ public static class ModHelperData
 
     public const string RepoOwner = "Greenphx9"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "BTD6_Mods"; // TODO add your repo name here, also in the download url in README.md
+
+    public const string SubPath = "NoParagonLimit";
+    public const string DllName = "NoParagonLimit.dll";
 }
